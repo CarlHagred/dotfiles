26e4a85aae0d9d2d7c4824940b7f98e017e8d20d
carlhagred <carl.hagred@gmail.com>
2025-12-09 15:22:11 +0100

    fix: show hidden and ignored files by default in snacks

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- for hidden files
+        ignored = true, -- for .gitignore files
+      },
+      -- explorer = {
+      --   enabled = false, -- Disables the file explorer part
+      -- },
+    },
+  },
+}
